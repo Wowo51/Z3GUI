31843fdcdbd2e9f4659b1f502f19e0b7ef29e679
Warren Harding <warrencharding@yahoo.com>
2025-10-30 09:43:46 -0600

    Add src/Z3WrapApp/App.xaml.cs

---
+++ src/Z3WrapApp/App.xaml.cs
@@ -0,0 +1,15 @@
+//Copyright Warren Harding 2025.
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Z3WrapApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
